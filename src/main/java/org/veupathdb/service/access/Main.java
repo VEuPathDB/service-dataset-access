c9e3319f6e892bdcd3b8577798fc012a948795ca
Elizabeth Harper <foxcapade@gmail.com>
2020-07-14 14:46:42 -0400

    update

--- src/main/java/org/veupathdb/service/access/Main.java
+++ src/main/java/org/veupathdb/service/access/Main.java
@@ -8,17 +8,17 @@ public class Main extends Server {
   public static void main(String[] args) {
     var server = new Main();
     server.enableAccountDB();
+    server.enableApplicationDB();
+    server.enableApplicationDB();
     server.start(args);
   }
 
   @Override
   protected ContainerResources newResourceConfig(Options options) {
     final var out =  new Resources(options);
-
-    // Enabled by default for debugging purposes, this should be removed when
-    // production ready.
-    out.property("jersey.config.server.tracing.type", "ALL")
-      .property("jersey.config.server.tracing.threshold", "VERBOSE");
+    out.enableAuth();
+    out.enableJerseyTrace();
+    out.enableCors();
 
     return out;
   }
