fb18237014e01b4ee98fb2e8a044d0481577ca53
Elizabeth Harper <foxcapade@gmail.com>
2020-07-21 14:15:14 -0400

    prepopulate caches

--- src/main/java/org/veupathdb/service/access/Main.java
+++ src/main/java/org/veupathdb/service/access/Main.java
@@ -3,6 +3,9 @@ package org.veupathdb.service.access;
 import org.veupathdb.lib.container.jaxrs.config.Options;
 import org.veupathdb.lib.container.jaxrs.server.ContainerResources;
 import org.veupathdb.lib.container.jaxrs.server.Server;
+import org.veupathdb.service.access.model.ApprovalStatusCache;
+import org.veupathdb.service.access.repo.ApprovalStatusRepo;
+import org.veupathdb.service.access.repo.RestrictionLevelRepo;
 
 public class Main extends Server {
   public static void main(String[] args) {
@@ -21,5 +24,15 @@ public class Main extends Server {
     out.enableCors();
 
     return out;
+  }
+
+  @Override
+  protected void postAcctDb() {
+    try {
+      ApprovalStatusRepo.Select.populateApprovalStatusCache();
+      RestrictionLevelRepo.Select.populateRestrictionLevelCache();
+    } catch (Throwable t) {
+      throw new RuntimeException(t);
+    }
   }
 }
